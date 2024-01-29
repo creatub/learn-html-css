eefbbc57ed1259d1e979fd28c86e6f885c4a9c21
KimSangHwi <blatherr@gmail.com>
2024-01-29 15:13:45 +0900

    Commit 3 javaScript practice & HW3 added

---
+++ Multicampus_javaScript/js/out.js
@@ -0,0 +1,10 @@
+//out.js
+
+//함수: 기능 정의
+function hello(name) {
+  alert('Hello? ' + name);
+}
+
+function go() {
+  location.href = 'http://www.google.com';
+}
---
+++ Multicampus_javaScript/js/out2.js
@@ -0,0 +1,14 @@
+//out2.js
+function laundry() {
+  console.log('==============');
+  console.log('1. 먼지 털자');
+  console.log('2. 물에 넣자');
+  console.log('3. 비누를 바른다');
+}
+
+const clean = function laundry() {
+  document.write("<hr color='red'>");
+  document.write('<ol>');
+  document.write('<li>aaaa</li>');
+  document.write('</ol>');
+};
